d7397417895a65215f3b11c6bdd01f898c97dfaa
Tian Khamez <tkhamez@gmail.com>
2019-01-19 16:02:43 +0100

    Added cache timeout to RoleProvider

--- src/RoleProvider.php
+++ src/RoleProvider.php
@@ -2,6 +2,7 @@
 namespace Brave\CoreConnector;
 
 use Brave\NeucoreApi\Api\ApplicationApi;
+use Brave\NeucoreApi\ApiException;
 use Brave\Sso\Basics\SessionHandlerInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Tkhamez\Slim\RoleAuth\RoleProviderInterface;
@@ -11,6 +12,11 @@ use Tkhamez\Slim\RoleAuth\RoleProviderInterface;
  */
 class RoleProvider implements RoleProviderInterface
 {
+    /**
+     * This role is always added.
+     */
+    const ROLE_ANY = 'role:any';
+
     /**
      * @var ApplicationApi
      */
@@ -37,35 +43,46 @@ class RoleProvider implements RoleProviderInterface
      */
     public function getRoles(ServerRequestInterface $request = null)
     {
-        #$this->session->set('coreGroups', null);
+        $roles = [self::ROLE_ANY];
 
         /* @var $eveAuth \Brave\Sso\Basics\EveAuthentication */
         $eveAuth = $this->session->get('eveAuth', null);
         if ($eveAuth === null) {
-            return [];
+            return $roles;
         }
 
-        $charId = $eveAuth->getCharacterId();
-
-        $coreGroups = $this->session->get('coreGroups', []);
-        if (isset($coreGroups[$charId])) {
-            return $coreGroups[$charId];
+        // try cache
+        $coreGroups = $this->session->get('coreGroups', null);
+        if (is_array($coreGroups) && $coreGroups['time'] > (time() - 60*60)) {
+            return $coreGroups['roles'];
         }
 
+        // get groups from Core
         try {
-            $groups = $this->api->groupsV1($charId);
-        } catch (\Exception $e) {
-            return [];
+            $groups = $this->api->groupsV1($eveAuth->getCharacterId());
+        } catch (ApiException $ae) {
+            // Don't log 404 character not found error from Core (response body is empty).
+            // If the URL was not found the response body contains HTML (from Core)
+            if ($ae->getCode() !== 404 || $ae->getResponseBody() !== '') {
+                error_log((string)$ae);
+            }
+            return $roles;
         }
-
-        $roles = [];
         foreach ($groups as $group) {
             $roles[] = $group->getName();
         }
 
-        $coreGroups[$charId] = $roles;
-        $this->session->set('coreGroups', $coreGroups);
+        // cache roles
+        $this->session->set('coreGroups', [
+            'time' => time(),
+            'roles' => $roles
+        ]);
 
         return $roles;
+    }
+
+    public function clear()
+    {
+        $this->session->set('coreGroups', null);
     }
 }
