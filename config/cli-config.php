6156626c0495151ebdd5c758bd6ce569ebf98b53
Tian Khamez <tkhamez@gmail.com>
2021-07-22 18:29:01 +0200

    Update dependencies, PHP 8 compatibility.

--- config/cli-config.php
+++ config/cli-config.php
@@ -5,18 +5,14 @@
 
 declare(strict_types=1);
 
+use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use EveSrp\Bootstrap;
-use Doctrine\DBAL\Tools\Console\Helper\ConnectionHelper;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
-use Symfony\Component\Console\Helper\HelperSet;
 
-require_once(__DIR__ . '/../vendor/autoload.php');
+require_once __DIR__ . '/../vendor/autoload.php';
 
 define('ROOT_DIR', realpath(__DIR__ . '/../'));
 
-$em = (new Bootstrap())->getContainer()->get(EntityManagerInterface::class);
-$helpers = new HelperSet(array(
-    'db' => new ConnectionHelper($em->getConnection()),
-    'em' => new EntityManagerHelper($em)
-));
+return ConsoleRunner::createHelperSet(
+    (new Bootstrap())->getContainer()->get(EntityManagerInterface::class)
+);
