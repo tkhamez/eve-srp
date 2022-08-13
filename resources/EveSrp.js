e3ffa522095245455a343ad067f0b82570a5785d
Tian Khamez <tkhamez@gmail.com>
2022-08-13 18:01:28 +0200

    Add choices.js for multi-selects.

--- resources/EveSrp.js
+++ resources/EveSrp.js
@@ -1,23 +1,34 @@
 import {Popover} from 'bootstrap';
+import Choices from "choices.js";
 
 window.addEventListener('load', () => {
     EveSrp.initPopover();
     EveSrp.initDeleteDivision();
+    EveSrp.initMultiselect();
     window.setInterval(EveSrp.ping, 300000); // 5 minutes
 });
 
 window.EveSrp = {
     initPopover: function () {
         [...document.querySelectorAll('[data-bs-toggle="popover"]')]
-            .map(popoverTriggerEl => new Popover(popoverTriggerEl, null))
+            .map(popoverTriggerEl => new Popover(popoverTriggerEl, null));
+    },
+
+    initMultiselect: function () {
+        document.querySelectorAll('.srp-multiselect').forEach((select) => {
+            new Choices(select, {
+                removeItemButton: true,
+                allowHTML: false,
+            });
+        });
     },
 
     initDeleteDivision: function () {
-        [...document.querySelectorAll('.delete-division')].map(button => {
+        document.querySelectorAll('.delete-division').forEach((button) => {
             button.addEventListener('click', function (evt) {
                 document.querySelector('#deleteModal input[name="id"]').value = evt.target.dataset.srpId;
             })
-        })
+        });
     },
 
     ping: function () {
