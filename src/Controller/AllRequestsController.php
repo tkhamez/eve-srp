63ed1ee3d3b3f0b1ed46759e21c52096a0455e04
Tian Khamez <tkhamez@gmail.com>
2023-04-11 21:29:05 +0200

    All requests list: add filter for user and pilot.

--- src/Controller/AllRequestsController.php
+++ src/Controller/AllRequestsController.php
@@ -7,8 +7,10 @@ namespace EveSrp\Controller;
 use EveSrp\Controller\Traits\RequestParameter;
 use EveSrp\Controller\Traits\TwigResponse;
 use EveSrp\Model\Permission;
+use EveSrp\Repository\CharacterRepository;
 use EveSrp\Repository\DivisionRepository;
 use EveSrp\Repository\RequestRepository;
+use EveSrp\Repository\UserRepository;
 use EveSrp\Security;
 use EveSrp\Misc\UserService;
 use Psr\Http\Message\ResponseInterface;
@@ -23,6 +25,8 @@ class AllRequestsController
     public function __construct(
         private RequestRepository $requestRepository,
         private DivisionRepository $divisionRepository,
+        private UserRepository $userRepository,
+        private CharacterRepository $characterRepository,
         private UserService $userService,
         Environment $environment
     ) {
@@ -31,49 +35,79 @@ class AllRequestsController
 
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
     {
-        $selectedStatus = (string)$this->paramGet($request, 'status');
-        $selectedDivision = (int)$this->paramGet($request, 'division', '0');
-        $currentPage = max(1, ((int)$this->paramGet($request, 'page', '1')));
-
         $divisions = $this->userService->getDivisionsWithRoles([Permission::REVIEW, Permission::PAY]);
 
-        // check division permission
-        $maySeeDivision = false;
-        foreach ($divisions as $division) {
-            if ($division->getId() === $selectedDivision) {
-                $maySeeDivision = true;
-                break;
-            }
-        }
-        if (
-            !$maySeeDivision &&
-            (
-                $selectedDivision !== -1 || // -1 = show requests without division ...
-                !$this->userService->hasRole(Security::GLOBAL_ADMIN) // ... but only for global admins
-            )
-        ) {
-            $selectedDivision = 0;
-        }
+        if ($this->paramGet($request, 'submit') !== null) {
+            $inputStatus = (string)$this->paramGet($request, 'status');
+            $inputDivision = (int)$this->paramGet($request, 'division', '0');
+            $inputUser = (string)$this->paramGet($request, 'user');
+            $inputPilot = (string)$this->paramGet($request, 'pilot');
+            $currentPage = max(1, ((int)$this->paramGet($request, 'page', '1')));
 
-        // variables for pager
-        $criteria = [
-            'status' => $selectedStatus,
-            'division' => $selectedDivision === -1 ? null : $selectedDivision
-        ];
-        $limit = 100;
-        $totalRequests = $this->requestRepository->count($criteria);
-        $totalPages = ceil($totalRequests / $limit);
-        $currentPage = min($totalPages, $currentPage);
-        $offset = max(0, ($limit * $currentPage) - $limit);
+            // check division permission
+            $maySeeDivision = false;
+            foreach ($divisions as $division) {
+                if ($division->getId() === $inputDivision) {
+                    $maySeeDivision = true;
+                    break;
+                }
+            }
+            if (
+                !$maySeeDivision &&
+                (
+                    $inputDivision !== -1 || // -1 = show requests without division ...
+                    !$this->userService->hasRole(Security::GLOBAL_ADMIN) // ... but only for global admins
+                )
+            ) {
+                $inputDivision = 0;
+            }
+
+            // Search criteria and variables for pager.
+            $criteria = [];
+            if ($inputStatus !== '') {
+                $criteria['status'] = $inputStatus;
+            }
+            if ($inputDivision !== 0) {
+                $criteria['division'] = $inputDivision === -1 ? null : $inputDivision;
+            }
+            if ($inputUser) {
+                $criteria['user'] = $this->getUserId($inputUser);
+            }
+            if ($inputPilot) {
+                $criteria['character'] = $this->getCharacterId($inputPilot);
+            }
+            $limit = 100;
+            $totalRequests = $this->requestRepository->count($criteria);
+            $totalPages = ceil($totalRequests / $limit);
+            $currentPage = min($totalPages, $currentPage);
+            $offset = max(0, ($limit * $currentPage) - $limit);
+
+            $pagerLink = "?division=$inputDivision&status=$inputStatus&user=$inputUser&pilot=$inputPilot&submit&page=";
+            $requests = $this->requestRepository->findBy($criteria, ['created' => 'DESC'], $limit, $offset);
+        }
 
         return $this->render($response, 'pages/all-requests.twig', [
-            'pagerCurrentPage' => $currentPage,
-            'pagerTotalPages' => $totalPages,
-            'pagerLink' => "?division=$selectedDivision&status=$selectedStatus&page=",
-            'requests' => $this->requestRepository->findBy($criteria, ['created' => 'ASC'], $limit, $offset),
             'divisions' => $divisions,
-            'selectedStatus' => $selectedStatus,
-            'selectedDivision' => $selectedDivision
+            'inputDivision' => $inputDivision ?? 0,
+            'inputStatus' => $inputStatus ?? '',
+            'inputUser' => $inputUser ?? null,
+            'inputPilot' => $inputPilot ?? null,
+            'requests' => $requests ?? [],
+            'pagerCurrentPage' => $currentPage ?? 0,
+            'pagerTotalPages' => $totalPages ?? 0,
+            'pagerLink' => $pagerLink ?? '',
         ]);
+    }
+
+    private function getUserId(string $name): ?int
+    {
+        $result = $this->userRepository->findOneBy(['name' => $name]);
+        return $result?->getId();
+    }
+
+    private function getCharacterId(string $name): ?int
+    {
+        $result = $this->characterRepository->findOneBy(['name' => $name]);
+        return $result?->getId();
     }
 }
