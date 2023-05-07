256d0c1b58a4e60b050d6440808ce48d801f730b
Tian Khamez <tkhamez@gmail.com>
2023-05-07 14:58:58 +0200

    Add GitHub release workflow and adjust build.sh.

--- build.sh
+++ build.sh
@@ -9,9 +9,6 @@ git checkout-index -a -f --prefix="${DIR}"/build/eve-srp/
 # Add required configuration
 {
   echo "EVE_SRP_DB_URL=sqlite:///:memory:"
-  echo "EVE_SRP_ENV=prod"
-  echo "EVE_SRP_PROVIDER="
-  echo "EVE_SRP_ROLE_GLOBAL_ADMIN="
   echo "EVE_SRP_SSO_CLIENT_ID="
   echo "EVE_SRP_SSO_CLIENT_SECRET="
   echo "EVE_SRP_SSO_REDIRECT_URI="
@@ -21,7 +18,7 @@ git checkout-index -a -f --prefix="${DIR}"/build/eve-srp/
 export UID
 docker-compose exec -u $UID eve_srp_php sh -c "cd build/eve-srp && composer install --no-dev --optimize-autoloader --no-interaction"
 docker-compose exec -u $UID eve_srp_php sh -c "cd build/eve-srp && bin/doctrine orm:generate-proxies"
-docker-compose exec -u $UID eve_srp_node sh -c "cd build/eve-srp && npm run build"
+docker-compose exec -u $UID eve_srp_node sh -c "cd build/eve-srp && npm install && npm run build"
 
 # Remove unnecessary files
 rm "${DIR}"/build/eve-srp/.editorconfig
@@ -37,10 +34,13 @@ rm "${DIR}"/build/eve-srp/phpunit.xml
 rm "${DIR}"/build/eve-srp/webpack.config.js
 rm "${DIR}"/build/eve-srp/config/.env
 rm "${DIR}"/build/eve-srp/config/docker-nginx.conf
+rm "${DIR}"/build/eve-srp/config/Dockerfile
 rm "${DIR}"/build/eve-srp/config/dockerfile-php80-fpm
 rm "${DIR}"/build/eve-srp/config/dockerfile-php81-fpm
 rm "${DIR}"/build/eve-srp/config/dockerfile-php82-fpm
 rm "${DIR}"/build/eve-srp/storage/.gitkeep
+rm -r "${DIR}"/build/eve-srp/.github
+rm -r "${DIR}"/build/eve-srp/node_modules
 rm -r "${DIR}"/build/eve-srp/resources
 rm -r "${DIR}"/build/eve-srp/tests
 
