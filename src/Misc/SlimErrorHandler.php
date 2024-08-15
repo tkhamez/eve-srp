363ad9a3cc78f749b26e465a29a1d7b018443986
Tian Khamez <tkhamez@gmail.com>
2024-08-15 17:28:48 +0200

    Change log for all HTTP exceptions.

--- src/Misc/SlimErrorHandler.php
+++ src/Misc/SlimErrorHandler.php
@@ -4,19 +4,16 @@ declare(strict_types=1);
 
 namespace EveSrp\Misc;
 
-use Slim\Exception\HttpMethodNotAllowedException;
-use Slim\Exception\HttpNotFoundException;
+use Slim\Exception\HttpException;
 use Slim\Handlers\ErrorHandler;
 
 class SlimErrorHandler extends ErrorHandler
 {
     protected function writeToErrorLog(): void
     {
-        if (
-            $this->exception instanceof HttpNotFoundException ||
-            $this->exception instanceof HttpMethodNotAllowedException
-        ) {
+        if ($this->exception instanceof HttpException) {
             $this->logError(
+                get_class($this->exception) . ': ' .
                 $this->exception->getMessage() .
                 ' Request: ' . $this->request->getMethod() . ' ' . $this->request->getUri()->getPath()
             );
